18778645c74b794e483fddaa090a14b2e37bb762
huntersfuntime <hunterorges@gmail.com>
2018-03-22 13:47:19 -0600

    Created pemdas challenge

---
+++ beginner/pemdas.py
@@ -0,0 +1,9 @@
+"""
+Take in two numbers, add them together. If the two numbers are the same, return the sum, and double it.
+
+sum_double(1, 2) -> 3
+sum_double(3, 2) -> 5
+sum_double(2, 2) -> 8
+
+"""
+def sum_double(a, b):
