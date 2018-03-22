967e80b0ce368b830fe278977d809db38b1aeb28
huntersfuntime <hunterorges@gmail.com>
2018-03-22 13:42:07 -0600

    monkey_business challenge created

---
+++ beginner/monkey_business.py
@@ -0,0 +1,21 @@
+""" There are two monkeys, if both monkeys are smiling. We are in trouble. If neither are smiling we are also in trouble. If just one is smiling then we are okay. 
+
+Get our method monkey_business working with our expected inputs.
+
+monkey_trouble(True, True) -> True
+monkey_trouble(False, False) -> True
+monkey_trouble(True, False) -> False
+monkey_trouble(False, True) -> False
+"""
+
+def monkey_business(a_smile, b_smile):
+
+
+
+
+
+
+monkey_business(True, True)
+monkey_business(False, False)
+monkey_business(True, False)
+monkey_business(False, True)
