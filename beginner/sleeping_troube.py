e896ceb027e572f1dcf863f15cc405b87d489650
huntersfuntime <hunterorges@gmail.com>
2018-03-22 14:58:53 -0600

    Sleeping trouble challenge

---
+++ beginner/sleeping_troube.py
@@ -0,0 +1,12 @@
+""" 
+New parents have a baby. The hour parameter is the current time in the range from 0 to 23. The parents have sleeping trouble if the baby is awake and crying before 7 and after 20. Return True if the parents have sleeping trouble.
+
+sleeping_trouble(True, 6) -> True
+sleeping_trouble(True, 7) -> False
+sleeping_trouble(False, 6) -> False
+sleeping_trouble(True, 20) -> False
+
+"""
+
+def sleeping_trouble(crying, hour):
+  
