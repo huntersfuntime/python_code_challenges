42f3b894022748ab74ab4f57eca03f870f570629
huntersfuntime <hunterorges@gmail.com>
2018-03-28 11:49:35 -0600

    Lambda cube challenge

---
+++ aLevel_one/payed.py
@@ -0,0 +1,9 @@
+""" Write a conditional to check if the status is payed. If payed print out "access permitted", otherwise print out "No access"
+"""
+from random import radint
+choice = randint(1, 10)
+
+if choice == 7:
+  print("Payed")
+else:
+  print("Unlucky")
---
+++ aLevel_two/lambda.py
@@ -0,0 +1,12 @@
+""" Write a lambda that accepts a single number and cubes it. Save it in a variable called cube. 
+cube(2) #8
+cube(3) #27
+cube(8) #512
+"""
+
+
+
+
+cube(2)
+cube(3)
+cube(8)
