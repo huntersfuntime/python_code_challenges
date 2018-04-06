5fdab6a96ba7b42868b6dc76a821ac696b1d2b36
huntersfuntime <hunterorges@gmail.com>
2018-04-06 09:17:28 -0600

    Not a hotdog code challenge

---
+++ aLevel_one/extremes.py
@@ -0,0 +1,18 @@
+""" Take in a iterables, and return a tuple containing min and max values.
+Hint. Use min/max function
+"""
+
+def extreme(chars):
+
+
+
+
+
+
+
+
+
+
+extremes([1,3,4,5,6]) #(1, 6)
+extremes((99,25,30,-2)) #(-7, 99)
+extremes("antartica") #('a', 'z')
---
+++ aLevel_one/not_hotdog.py
@@ -0,0 +1,12 @@
+""" Check to see if the food is a hotdog, if it is return the hotdog. If not, return not hotdog. """
+
+
+def not_hotdog(food):
+
+
+
+
+print(not_hotdog('hotdog'))
+print(not_hotdog('popsicle'))
+print(not_hotdog('salad'))
+print(not_hotdog('jalapeno hotdog'))
